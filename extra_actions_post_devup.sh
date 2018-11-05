d01cd8bea5199d891e2dbd4ecc7aa3ab22df77d2
jfavellar90 <jhony.avella@edunext.co>
2018-11-05 08:39:58 -0500

    Adding ipdb for dev purposes

--- extra_actions_post_devup.sh
+++ extra_actions_post_devup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Some actions to perform after "make dev.up"
-echo "Installin campus plugin on LMS"
-docker-compose exec lms bash -c "source /edx/app/edxapp/devstack.sh && cd /edx/src/campusromero-openedx-extensions && pip install -e ."
-echo "Installin campus plugin on CMS"
-docker-compose exec studio bash -c "source /edx/app/edxapp/devstack.sh && cd /edx/src/campusromero-openedx-extensions && pip install -e ."
+echo "Installing campus plugin on LMS"
+docker-compose exec lms bash -c "source /edx/app/edxapp/devstack.sh && cd /edx/src/campusromero-openedx-extensions && pip install -e . && pip install ipdb"
+echo "Installing campus plugin on CMS"
+docker-compose exec studio bash -c "source /edx/app/edxapp/devstack.sh && cd /edx/src/campusromero-openedx-extensions && pip install -e . && pip install ipdb"
