48a72e487516500cf995a6dc9486427590504e58
constanza <constanza.abarca@edunext.co>
2019-10-09 16:50:51 -0400

    Adding config to clone the plugin repo in the devstack

--- extra_actions_post_devup.sh
+++ extra_actions_post_devup.sh
@@ -2,5 +2,3 @@
 # Some actions to perform after "make dev.up"
 echo "Installin llpa plugin on LMS"
 docker-compose exec lms bash -c "source /edx/app/edxapp/devstack.sh && cd /edx/src/llpa_openedx_extensions && pip install -e ."
-echo "Installin llpa plugin on CMS"
-docker-compose exec studio bash -c "source /edx/app/edxapp/devstack.sh && cd /edx/src/llpa_openedx_extensions && pip install -e ."
