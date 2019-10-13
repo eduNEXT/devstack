c737f1cf9d947bed87b05464ca4a65c44d016992
jfavellar90 <jhony.avella@edunext.co>
2019-10-13 13:26:42 -0500

    The plugin must be always installed in CMS on devstack since it's installed on CMS in production and we want to check there is no side effects on CMS functionality.

--- extra_actions_post_devup.sh
+++ extra_actions_post_devup.sh
@@ -2,3 +2,5 @@
 # Some actions to perform after "make dev.up"
 echo "Installin llpa plugin on LMS"
 docker-compose exec lms bash -c "source /edx/app/edxapp/devstack.sh && cd /edx/src/llpa_openedx_extensions && pip install -e ."
+echo "Installin llpa plugin on CMS"
+docker-compose exec studio bash -c "source /edx/app/edxapp/devstack.sh && cd /edx/src/llpa_openedx_extensions && pip install -e ."
